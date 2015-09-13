29b3be2a1edda3184760207eaeab0c4fdcd99819
shenry <shenry@handycodejob.com>
2015-09-12 20:59:14 -0700

    uncomment ord call to see if it fixes the cert error

--- src/wedding/settings/production.py
+++ src/wedding/settings/production.py
@@ -42,7 +42,7 @@ AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
 AWS_S3_HOST = 's3-us-west-2.amazonaws.com'
 AWS_QUERYSTRING_AUTH = False
-#AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()
+AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()
 
 # AWS cache settings, don't change unless you know what you're doing:
 AWS_EXPIRY = 60 * 60 * 24 * 7
