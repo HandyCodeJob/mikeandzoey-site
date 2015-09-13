d94e2128e15ed459a5f094c486f351253c2f97ae
shenry <shenry@handycodejob.com>
2015-09-12 19:31:59 -0700

    edit static url loc - needed static/

--- src/wedding/settings/production.py
+++ src/wedding/settings/production.py
@@ -37,7 +37,7 @@ AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CALLING_FORMAT = 'boto.s3.connection.OrdinaryCallingFormat'
 
-STATIC_URL = 'https://mikeandzoey.com.s3.amazonaws.com/'
+STATIC_URL = 'https://mikeandzoey.com.s3.amazonaws.com/static/'
 
 # see http://developer.yahoo.com/performance/rules.html#expires
 AWS_HEADERS = {
